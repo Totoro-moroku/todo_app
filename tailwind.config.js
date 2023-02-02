63dadcc9c8f0716aa630ff108bbba43449d25160
Totoro-moroku <shinsi4545@gmail.com>
2023-02-02 17:49:50 +0900

    :gear: 適用するフォルダーを増やしました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './recoil/*.{ts,js}',
+    './recoil/**/*.{ts,js}',
   ],
   theme: {
     extend: {

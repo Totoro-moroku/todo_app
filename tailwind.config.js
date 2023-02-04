f001de36123abda8127479909a8bf2471dfec0b3
Totoro-moroku <shinsi4545@gmail.com>
2023-02-05 00:55:15 +0900

    :bug: tailwindの適用するファイルのpathの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './recoil/*.{ts,js}',
-    './recoil/**/*.{ts,js}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/recoil/*.{ts,js}',
+    './src/recoil/**/*.{ts,js}',
   ],
   theme: {
     extend: {

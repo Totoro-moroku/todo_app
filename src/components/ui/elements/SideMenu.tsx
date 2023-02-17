3059d0a199ed90ecbc0bc493cc0047fa3b3e5ad2
Totoro-moroku <shinsi4545@gmail.com>
2023-02-17 18:05:02 +0900

    ツールチップ導入

--- src/components/ui/elements/SideMenu.tsx
+++ src/components/ui/elements/SideMenu.tsx
@@ -1,5 +1,16 @@
-const SideMenu = () => {
-  return <div>SideMenu</div>
+import { FC, ReactNode } from 'react'
+
+type SideMenuPorps = {
+  children: ReactNode
+  className?: string
+}
+
+const SideMenu: FC<SideMenuPorps> = ({ className, children }) => {
+  return (
+    <div className={`${className} bg-slate-0 h-screen w-52 border-r-2`}>
+      {children}
+    </div>
+  )
 }
 
 export default SideMenu
